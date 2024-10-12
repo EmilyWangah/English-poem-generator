d829b618d4d9ecd0c07c962aaa35934df22db5fa
EmilyWangah <emilywangah@gmail.com>
2024-10-12 20:19:08 +0200

    Added basic JS and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert('Hello world');
+function generatePoem(event){
+    event.preventDefault();
+
+    let poemElement =document.querySelector("#poem");
+
+    new Typewriter("#poem", {
+      strings: [ "The poem will go here"],
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+
+    
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
